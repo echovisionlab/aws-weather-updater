0127442a36e315ff3301a1296446872e631c1abb
state303 <state303@dsub.io>
2024-03-24 00:29:30 +0900

    test: reverts to actual url

--- pkg/fetch/fetch_test.go
+++ pkg/fetch/fetch_test.go
@@ -2,11 +2,9 @@ package fetch
 
 import (
 	"context"
-	"github.com/echovisionlab/aws-weather-updater/internal/testutil"
 	"github.com/echovisionlab/aws-weather-updater/pkg/browser"
 	"github.com/go-rod/rod/lib/proto"
 	"github.com/stretchr/testify/assert"
-	"path"
 	"testing"
 	"time"
 )
@@ -35,9 +33,6 @@ func Test_StationsAndRecords(t *testing.T) {
 		return
 	}
 
-	backupUrl := measurementBaseUrl
-	measurementBaseUrl = path.Join(testutil.TestDataPath, "test_fetch.html")
-
 	t.Cleanup(func() {
 		if p != nil {
 			p.MustClose()
@@ -48,7 +43,6 @@ func Test_StationsAndRecords(t *testing.T) {
 		if l != nil {
 			l.Cleanup()
 		}
-		measurementBaseUrl = backupUrl
 	})
 
 	t.Run("must report context exceeded", func(t *testing.T) {
