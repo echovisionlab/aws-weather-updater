c43078914e484eb973448eb839c5bea3c0379a33
state303 <state303@dsub.io>
2024-03-24 16:38:45 +0900

    fix: pages will no longer be shared within fetch tests

--- pkg/fetch/fetch_test.go
+++ pkg/fetch/fetch_test.go
@@ -3,7 +3,6 @@ package fetch
 import (
 	"context"
 	"github.com/echovisionlab/aws-weather-updater/pkg/browser"
-	"github.com/go-rod/rod/lib/proto"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -17,26 +16,7 @@ func Test_StationsAndRecords(t *testing.T) {
 		return
 	}
 
-	p, err := b.Page(proto.TargetCreateTarget{
-		URL:                     "",
-		Width:                   nil,
-		Height:                  nil,
-		BrowserContextID:        "",
-		EnableBeginFrameControl: false,
-		NewWindow:               false,
-		Background:              false,
-		ForTab:                  false,
-	})
-
-	if err != nil {
-		t.Fail()
-		return
-	}
-
 	t.Cleanup(func() {
-		if p != nil {
-			p.MustClose()
-		}
 		if b != nil {
 			b.MustClose()
 		}
@@ -48,6 +28,9 @@ func Test_StationsAndRecords(t *testing.T) {
 	t.Run("must report context exceeded", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*10)
 		defer cancel()
+
+		p := b.MustPage()
+		defer p.MustClose()
 		result, err := StationsAndRecords(ctx, p, time.Now())
 		assert.Nil(t, result)
 		assert.Error(t, err)
@@ -56,15 +39,28 @@ func Test_StationsAndRecords(t *testing.T) {
 	t.Run("must report context cancelled", func(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 		cancel()
+		p := b.MustPage()
+		defer p.MustClose()
 		result, err := StationsAndRecords(ctx, p, time.Now())
 		assert.Nil(t, result)
 		assert.Error(t, err)
 		assert.ErrorIs(t, err, context.Canceled)
 	})
 	t.Run("must fetch", func(t *testing.T) {
+		p := b.MustPage()
+		defer p.MustClose()
 		result, err := StationsAndRecords(context.Background(), p, time.Now())
 		assert.NoError(t, err)
-		assert.NotEmpty(t, result.Records())
+		assert.NotNil(t, result)
+		if t.Failed() {
+			return
+		}
+		assert.NotNil(t, result.Stations())
+		assert.NotNil(t, result.Records())
+		if t.Failed() {
+			return
+		}
 		assert.NotEmpty(t, result.Stations())
+		assert.NotEmpty(t, result.Records())
 	})
 }
